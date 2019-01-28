702fa531da243f20db8f554d82d8758a7ee5c54f
tenarasu <46886361+Thenarasu@users.noreply.github.com>
2019-01-28 10:25:28 +0530

    Update TEQ.asm

--- TEQ/TEQ.asm
+++ TEQ/TEQ.asm
@@ -17,12 +17,12 @@
 		MOV R5,#4			 ;R5 = 4 Move value to R5 Register
 
 LOOP1		LDR R2,[R0],#4 			 ;R0  Have some address that R0 address have some value that value load to R2 register
-		LDR	R3,[R1],#4		 ;R3  Have some address that R3 address have some value that value load to R3 register
+		LDR R3,[R1],#4			 ;R3  Have some address that R3 address have some value that value load to R3 register
 		TEQ R2,R3			 ;Test if(R2==R3)
-		BNE	LOOP			 ;When CPSR status Z = 1 set that time exit form the loop and this is forward loop
+		BNE LOOP			 ;When CPSR status Z = 1 set that time exit form the loop and this is forward loop
 		STR R2,[R4],#4 			 ;R2 value store to R4 address
 		SUBS R5,#1			 ;sutraction	R5 -=1
-		BNE	LOOP1			 ;When CPSR status Z = 1 set that time exit form the loop and this is backward loop
+		BNE LOOP1			 ;When CPSR status Z = 1 set that time exit form the loop and this is backward loop
 LOOP						 ;End of the forward 
 		
 L       	B	L			 ;like while(1);
