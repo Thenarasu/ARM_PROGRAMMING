7cc71796105cb4d3908af33546372d30056d8d50
tenarasu <46886361+Thenarasu@users.noreply.github.com>
2019-02-18 17:48:11 -0800

    Branch if carry flag is clear

--- Branch_instructions/BCC.s
+++ Branch_instructions/BCC.s
@@ -5,6 +5,6 @@
 
 LOOP 	
 		ADDS R0,R1			; add values R0+=R1
-		BCC	 LOOP			; When CPSR update Carry bit that time exit from the loop while(c != 1)
-L 		B	 L				; Always branch
+		BCC  LOOP			; When CPSR update Carry bit that time exit from the loop while(c != 1)
+L 		B    L	         		; Always branch
 		END
