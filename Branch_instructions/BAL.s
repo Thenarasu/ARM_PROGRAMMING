d5719ccdf258b8ce3b2456f8074b5cee14eb727c
tenarasu <46886361+Thenarasu@users.noreply.github.com>
2019-02-18 17:46:40 -0800

    Branch Always

--- Branch_instructions/BAL.s
+++ Branch_instructions/BAL.s
@@ -2,7 +2,7 @@
 			ENTRY
 LOOP		
 			LDR R0,=0X01 			;Load the value to R0 register  
-			LDR	R1,=0X03			;load the value to R1 register 
+			LDR R1,=0X03			;load the value to R1 register 
 			ADDS R3,R0,R1			;R3 = R0 + R1
-			BAL	LOOP				;Continusly execute the loop while(1)
+			BAL	LOOP			;Continusly execute the loop while(1)
 			END
