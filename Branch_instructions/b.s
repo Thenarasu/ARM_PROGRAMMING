71a53d029b47725b2e5b9dbef5b347937a15ea07
tenarasu <46886361+Thenarasu@users.noreply.github.com>
2019-02-18 17:56:26 -0800

    Branch Always

--- Branch_instructions/b.s
+++ Branch_instructions/b.s
@@ -2,9 +2,10 @@
 			ENTRY
 			
 			
-LOOP1		LDR R1,=0X80000000		;Load the value to R1 register
+LOOP1		
+			LDR R1,=0X80000000		;Load the value to R1 register
 			LDR R2,=0X00000001		;Load the value to R2 register
 			ADDS R3,R1,R2			;R3 = R1+R2 
 			SUBS R4,R1,R2			;R4 = R1+R2
-			B LOOP1					;Continusly execute the loop while(1)
+			B LOOP1				;Continusly execute the loop while(1)
 			END
