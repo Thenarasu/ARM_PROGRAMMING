1b37269385827ffce39f811f7fb80222e15e22f7
tenarasu <46886361+Thenarasu@users.noreply.github.com>
2019-02-18 17:49:32 -0800

    Branch if Equal

--- Branch_instructions/BEQ.s
+++ Branch_instructions/BEQ.s
@@ -3,8 +3,9 @@
 					
 			LDR R0,=0X01		;load the value to R0 register
 			LDR R1,=0X01		;load the value to R1 register
-LOOP1		CMP R0,R1			; Compare 2 values 
+LOOP1			
+			CMP R0,R1			; Compare 2 values 
 			ADD	R0,#1
 			BEQ	LOOP1			; When both values are not  eqaual that time exit from the loop
-L			B	L				;while(1)	
-			END					;End the program
+L			B	L			;while(1)	
+			END				;End the program
