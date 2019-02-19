ee072caeb07cace97f59772085b884aafbc29f50
tenarasu <46886361+Thenarasu@users.noreply.github.com>
2019-02-18 17:53:33 -0800

    Branch if not equal

--- Branch_instructions/BNE.s
+++ Branch_instructions/BNE.s
@@ -1,8 +1,8 @@
 		AREA BRANCH,CODE,READONLY
 		ENTRY
-		MOV R0,#5
+		MOV R0,#5		;Move the value R0 register
 LOOP
-		SUBS R0,#1
+		SUBS R0,#1		R0-=1
 		BNE LOOP		;when z flag set that time exit from the loop
-L		B	L
+L		B	L		;Always branch
 		END
