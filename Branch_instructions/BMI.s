eb306e29896dc9117c3ce0701491d9e9758d29e6
tenarasu <46886361+Thenarasu@users.noreply.github.com>
2019-02-18 17:51:54 -0800

    Branch on negative

--- Branch_instructions/BMI.s
+++ Branch_instructions/BMI.s
@@ -2,10 +2,10 @@
 		ENTRY
 		
 		LDR R0,=0X8FFFFFFE	;Load the value R0 register	
-		LDR R1,=1			;Load the value R1 register
+		LDR R1,=1		;Load the value R1 register
 
 LOOP	
 		ADDS R0,R1
-		BMI	 LOOP		; When CPSR not set negative bit that time exit from the loop 
-L		B	 L			; Always branch
+		BMI  LOOP		; When CPSR not set negative bit that time exit from the loop 
+L		B    L			; Always branch
 		END
