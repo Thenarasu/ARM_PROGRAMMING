9af632f4b9ed9c563c676dbfc845e6b7428c3838
tenarasu <46886361+Thenarasu@users.noreply.github.com>
2019-02-18 17:55:36 -0800

    Branch if overflow flag is clear

--- Branch_instructions/BVC.s
+++ Branch_instructions/BVC.s
@@ -1,11 +1,11 @@
 		AREA BRANCH,CODE,READONLY
 		ENTRY
 		
-		LDR	R0,=0X7FFFFFFF	; Load the value R0 register
+		LDR R0,=0X7FFFFFFF	; Load the value R0 register
 		LDR R1,=0X80000000	; Load the value R1 register
 		
 LOOP	
 		ADDS R0,R1
-		BVC	 LOOP		; When the cpsr register take overflow bit that time exit from the loop
-L	    B	 L			; always branch
+		BVC  LOOP		; When the cpsr register take overflow bit that time exit from the loop
+L	        B    L			; always branch
 		END
